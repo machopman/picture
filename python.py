7a22f6e8944e54461de048d4bd2ba6747390c417
macho <machopman_shop@hotmail.com>
2018-05-03 03:50:42 +0700

    wdadfwe

--- python.py
+++ python.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import (MessageEvent, TextMessage, TextSendMessage, ImageSendMessage)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, TemplateSendMessage,
+                            CarouselTemplate, CarouselColumn, PostbackTemplateAction, MessageTemplateAction,
+                            URITemplateAction)
 
 app = Flask(__name__)
 
@@ -41,6 +43,56 @@ def handle_message(event):
                 preview_image_url='https://imagemovie.herokuapp.com/pic1.jpg'
             )
             line_bot_api.push_message('U7183997e3e85a10d8c5f1f3925825016', image_message)
+
+        if event.message.text=='ปุ่ม':
+            message = TemplateSendMessage(
+                alt_text='Carousel template',
+                template=CarouselTemplate(
+                    columns=[
+                        CarouselColumn(
+                            thumbnail_image_url='https://imagemovie.herokuapp.com/tt3501632.jpg',
+                            title='this is menu1',
+                            text='description1',
+                            actions=[
+                                PostbackTemplateAction(
+                                    label='postback1',
+                                    text='postback text1',
+                                    data='action=buy&itemid=1'
+                                ),
+                                MessageTemplateAction(
+                                    label='message1',
+                                    text='message text1'
+                                ),
+                                URITemplateAction(
+                                    label='uri1',
+                                    uri='http://mandm.plearnjai.com/'
+                                )
+                            ]
+                        ),
+                        CarouselColumn(
+                            thumbnail_image_url='https://imagemovie.herokuapp.com/tt3501632.jpg',
+                            title='this is menu2',
+                            text='description2',
+                            actions=[
+                                PostbackTemplateAction(
+                                    label='postback2',
+                                    text='postback text2',
+                                    data='action=buy&itemid=2'
+                                ),
+                                MessageTemplateAction(
+                                    label='message2',
+                                    text='message text2'
+                                ),
+                                URITemplateAction(
+                                    label='uri2',
+                                    uri='http://mandm.plearnjai.com/'
+                                )
+                            ]
+                        )
+                    ]
+                )
+            )
+            line_bot_api.reply_message(event.reply_token, message)
 
 
 
